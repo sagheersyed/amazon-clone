9de746dd7aa41cf8bdf440e7dc91c07251d36d8c
sagheersyed <67121507+sagheersyed@users.noreply.github.com>
2021-03-28 01:23:35 +0500

    Update Cart.jsx

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -8,7 +8,6 @@ function Cart() {
        </Container>
     )
 }
-
 export default Cart
 const Container = styled.div`
 background-image:url('https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg')
